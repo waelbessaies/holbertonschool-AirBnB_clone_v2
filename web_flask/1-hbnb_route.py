7c5e08aa0f90df764aa60d97e5c76a888a5fdd7f
root <root@LAPTOP-I8GVERUB.localdomain>
2022-12-02 16:12:03 +0100

    task 1 and 2

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" a script that starts a Flask web application"""
+"""starts a flask web application
+tuned on 0.0.0.0 and port 5000"""
 
 from flask import Flask
 
@@ -7,14 +8,12 @@ app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
-def hbnb():
-    """display hello hbnb"""
+def hello_hbnb():
     return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    """this displays HBNB"""
     return "HBNB"
 
 
--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""a script that starts a Flask web application"""
+"""starts a flask web application
+tuned on 0.0.0.0 and port 5000"""
 
 from flask import Flask
 
@@ -8,19 +9,16 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hbnb():
-    """display hello hbnb"""
     return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    """this displays HBNB"""
     return "HBNB"
 
 
 @app.route("/c/<text>", strict_slashes=False)
 def c(text):
-    """this will displays 'c'"""
     text = text.replace("_", " ")
     return "C {}".format(text)
 
